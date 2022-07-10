c4bfa59326b68d348b9a3c119348bbc4e51a722b
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-10 11:02:59 -0500

    Ultimate power

--- Sleepless2.5/Assets/Scripts/Game/Player/Ultimate/UltimateHandler.cs
+++ Sleepless2.5/Assets/Scripts/Game/Player/Ultimate/UltimateHandler.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(IUltimate))]
 public class UltimateHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private KeyCode _ultimateKey = KeyCode.U;
+    [SerializeField] private float _requiredPowerAmount = 100;
+    private float _power;
+
+    private IUltimate _ultimate;
+
+    private void Awake()
     {
-        
+        _ultimate = GetComponent<IUltimate>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(_ultimateKey) && _power >= _requiredPowerAmount)
+            _ultimate.Execute();
     }
+
+    public void AddPower(float amount)
+    {
+        _power += amount;
+        if (_power >= _requiredPowerAmount)
+            _power = _requiredPowerAmount;
+    }
+
+
 }
