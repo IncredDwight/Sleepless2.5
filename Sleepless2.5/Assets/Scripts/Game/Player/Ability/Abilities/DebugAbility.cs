e96d36b43814fc93a05694d6a34900ed37ee3698
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-09 19:52:04 -0500

    Test ability

--- Sleepless2.5/Assets/Scripts/Game/Player/Ability/Abilities/DebugAbility.cs
+++ Sleepless2.5/Assets/Scripts/Game/Player/Ability/Abilities/DebugAbility.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugAbility : MonoBehaviour
+public class DebugAbility : MonoBehaviour, IAbility
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Execute()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Test ability");
     }
 }
