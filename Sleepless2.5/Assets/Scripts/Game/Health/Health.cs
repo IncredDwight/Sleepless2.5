661ebb02b4fa5ebd753cd0fa2b1060299d8cb3b9
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-10 21:23:50 -0500

    Player health display script

--- Sleepless2.5/Assets/Scripts/Game/Health/Health.cs
+++ Sleepless2.5/Assets/Scripts/Game/Health/Health.cs
@@ -7,16 +7,30 @@ public abstract class Health : MonoBehaviour, ITakeDamage, IHeal
     [SerializeField] private float _maxHealth;
     private float _health;
 
+    public delegate void HealthChanged(float health);
+    public event HealthChanged OnHealthChanged;
+
     private void Awake()
     {
         _health = _maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+            TakeDamage(10);
     }
 
     public void TakeDamage(float damage)
     {
         _health -= damage;
         if (_health <= 0)
+        {
+            _health = 0;
             Die();
+        }
+
+        OnHealthChanged?.Invoke(_health);
     }
 
     public void Heal(float heal)
@@ -24,6 +38,13 @@ public abstract class Health : MonoBehaviour, ITakeDamage, IHeal
         _health += heal;
         if (_health > _maxHealth)
             _health = _maxHealth;
+
+        OnHealthChanged?.Invoke(_health);
+    }
+
+    public float GetHealth()
+    {
+        return _health;
     }
 
     protected abstract void Die();
--- Sleepless2.5/Assets/Scripts/UI/Player/HealthDisplay.cs
+++ Sleepless2.5/Assets/Scripts/UI/Player/HealthDisplay.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class HealthDisplay : MonoBehaviour
 {
+    private PlayerHealth _playerHealth;
+    private TextMeshProUGUI _healthDisplay;
 
+    private void Awake()
+    {
+        _healthDisplay = GetComponent<TextMeshProUGUI>();
+
+        _playerHealth = FindObjectOfType<PlayerHealth>();
+        _playerHealth.OnHealthChanged += ChangeDisplay;
+
+        ChangeDisplay(_playerHealth.GetHealth());
+    }
+
+    public void ChangeDisplay(float health)
+    {
+        _healthDisplay.text = health.ToString();
+    }
+
+    private void OnDestroy()
+    {
+        _playerHealth.OnHealthChanged -= ChangeDisplay;
+    }
 }
