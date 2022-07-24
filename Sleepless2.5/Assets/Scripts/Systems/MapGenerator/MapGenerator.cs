82e1abdd0114a79735cb589b66dcd5d2fa5673bc
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-24 15:37:08 -0500

    Map tiles

--- Sleepless2.5/Assets/Scripts/Systems/MapGenerator/MapGenerator.cs
+++ Sleepless2.5/Assets/Scripts/Systems/MapGenerator/MapGenerator.cs
@@ -6,23 +6,27 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject[] _tiles;
 
-    [SerializeField]private Vector2 _minCoordinate;
-    private Vector2 _maxCoordinate;
+    [SerializeField] private Vector2 _minCoordinate;
+    [SerializeField] private Vector2 _maxCoordinate;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
             Spawn();
+        
     }
 
     private void Spawn()
     {
         Vector2 spawnPosition = new Vector2(Random.Range(_minCoordinate.x, _maxCoordinate.x), Random.Range(_minCoordinate.y, _maxCoordinate.y));
         GameObject tile = Instantiate(_tiles[Random.Range(0, _tiles.Length)], spawnPosition, Quaternion.identity);
-        if (Physics2D.OverlapBoxAll(tile.transform.position, new Vector2(20, 20), 0).Length > 4)
-        {
-            Destroy(tile);
-            Spawn();
-        }
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(tile.transform.position, new Vector2(5, 5), 0);
+        foreach(Collider2D collider in colliders)
+            if(collider.transform.parent != tile.transform)
+            {
+                Destroy(tile);
+                
+                //Spawn();
+            }    
     }
 }
