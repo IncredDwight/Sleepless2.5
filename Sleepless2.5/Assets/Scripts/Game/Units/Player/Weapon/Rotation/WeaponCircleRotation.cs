f1e507af48ad902df6fe5fb31597b7820f2cfbd9
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-25 15:40:25 -0500

    Circle rotation updated
    Default shooting point position added
    Shooting point rotation

--- Sleepless2.5/Assets/Scripts/Game/Units/Player/Weapon/Rotation/WeaponCircleRotation.cs
+++ Sleepless2.5/Assets/Scripts/Game/Units/Player/Weapon/Rotation/WeaponCircleRotation.cs
@@ -7,17 +7,26 @@ public class WeaponCircleRotation : MonoBehaviour
     [SerializeField] private Joystick _shootingJoystick;
     [SerializeField] private Transform _shootingPoint;
 
-    private float _radius = 3;
+    [SerializeField] private float _radius = 3;
+
+    private float _defaultAngle = Mathf.PI / 2;
+    private float _inputAngle;
 
     private void Update()
     {
-        Rotate();
+        GetInput();
+        Rotate((_shootingJoystick.GetDirection() != Vector2.zero) ? _inputAngle : _defaultAngle);
     }
 
-    private void Rotate()
+    private void Rotate(float angle)
     {
-        float angle = Mathf.Atan2(_shootingJoystick.GetDirection().normalized.y, _shootingJoystick.GetDirection().normalized.x);
         _shootingPoint.localPosition = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _radius;
+        _shootingPoint.localRotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+    }
+
+    private void GetInput()
+    {
+        _inputAngle = Mathf.Atan2(_shootingJoystick.GetDirection().normalized.y, _shootingJoystick.GetDirection().normalized.x);
     }
 
 }
