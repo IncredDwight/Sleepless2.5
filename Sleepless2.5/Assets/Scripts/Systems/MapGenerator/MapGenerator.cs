6c708052b482e881d750450b8255792f9af659ab
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-26 15:36:11 -0500

    Space between tiles

--- Sleepless2.5/Assets/Scripts/Systems/MapGenerator/MapGenerator.cs
+++ Sleepless2.5/Assets/Scripts/Systems/MapGenerator/MapGenerator.cs
@@ -8,6 +8,7 @@ public class MapGenerator : MonoBehaviour
 
     [SerializeField] private Vector2 _minCoordinate;
     [SerializeField] private Vector2 _maxCoordinate;
+    [SerializeField] private float _space = 1.5f;
 
     private void Update()
     {
@@ -20,13 +21,29 @@ public class MapGenerator : MonoBehaviour
     {
         Vector2 spawnPosition = new Vector2(Random.Range(_minCoordinate.x, _maxCoordinate.x), Random.Range(_minCoordinate.y, _maxCoordinate.y));
         GameObject tile = Instantiate(_tiles[Random.Range(0, _tiles.Length)], spawnPosition, Quaternion.identity);
-        Collider2D[] colliders = Physics2D.OverlapBoxAll(tile.transform.position, new Vector2(5, 5), 0);
+        Bounds bounds = GetBounds(tile.transform);
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(tile.transform.position, bounds.size * _space, 0);
         foreach(Collider2D collider in colliders)
             if(collider.transform.parent != tile.transform)
             {
                 Destroy(tile);
-                
-                //Spawn();
+
             }    
+    }
+
+    private Bounds GetBounds(Transform tile)
+    {
+        Bounds bounds = new Bounds();
+        foreach(Transform child in tile)
+        {
+            bounds.Encapsulate(child.GetComponent<Collider2D>().bounds);
+        }
+
+        return bounds;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawCube(transform.position, _maxCoordinate - _minCoordinate);
     }
 }
