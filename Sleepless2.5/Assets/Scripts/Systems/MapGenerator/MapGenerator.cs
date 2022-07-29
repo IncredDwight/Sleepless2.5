4817000d2a74408f83ac766d50c9c0b6837ef9c1
Yaroslav_Valchyshen <yaravirus86@gmail.com>
2022-07-29 18:42:09 -0500

    Automatic spawn

--- Sleepless2.5/Assets/Scripts/Systems/MapGenerator/MapGenerator.cs
+++ Sleepless2.5/Assets/Scripts/Systems/MapGenerator/MapGenerator.cs
@@ -9,17 +9,33 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private Vector2 _minCoordinate;
     [SerializeField] private Vector2 _maxCoordinate;
     [SerializeField] private float _space = 1.5f;
+    [SerializeField] private float _density = 4;
+
+    private void Start()
+    {
+        for (int k = 0; k < _density; k++)
+        {
+            for (float x = _minCoordinate.x; x < _maxCoordinate.x; x += Random.Range(5, 40))
+            {
+                for (float y = _minCoordinate.y; y < _maxCoordinate.y; y += Random.Range(20, 40))
+                {
+                    Spawn(x, y);
+                }
+            }
+        }
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
-            Spawn();
+            Spawn(0, 0);
         
     }
 
-    private void Spawn()
+    private void Spawn(float x, float y)
     {
-        Vector2 spawnPosition = new Vector2(Random.Range(_minCoordinate.x, _maxCoordinate.x), Random.Range(_minCoordinate.y, _maxCoordinate.y));
+        Debug.Log($"Try {x} {y}");
+        Vector2 spawnPosition = new Vector2(x, y);// new Vector2(Random.Range(_minCoordinate.x, _maxCoordinate.x), Random.Range(_minCoordinate.y, _maxCoordinate.y));
         GameObject tile = Instantiate(_tiles[Random.Range(0, _tiles.Length)], spawnPosition, Quaternion.identity);
 
         Vector2 minPoint = TransformExtremums.GetMinPoint(tile.transform.GetChild(0));
